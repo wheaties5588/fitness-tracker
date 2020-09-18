3326a57884d5371c18897a22a7a3b71d5f758da2
Matt Wieciech <matt.wieciech@gmail.com>
2020-09-17 22:57:08 -0400

    Created API routes and edited serverjs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,32 +5,34 @@ module.exports = function (app) {
     app.get("/api/workouts", function (req, res) {
         Workout.find()
             .then(data => {
-                res.json(data)
+                res.json(data);
             })
             .catch(err => {
-                res.json(err)
+                res.json(err);
             })
     });
 
     app.post("/api/workouts", function (req, res) {
         Workout.create({})
-            .then(data => res.json(data))
+            .then(data => {
+                res.json(data);
+            })
             .catch(err => {
-                console.log("err", err)
-                res.json(err)
+                res.json(err);
             })
     });
 
-    app.put("/api/workouts/:id", ({ body, params }, res) => {
+    app.put("/api/workouts/:id", (req, res) => {
         Workout.findByIdAndUpdate(
-            params.id,
-            { $push: { exercises: body } },
-            { new: true, runValidators: true }
+            req.params.id,
+            { $push: { exercises: req.body } },
+            { new: true }
         )
-            .then(data => res.json(data))
+            .then(data => {
+                res.json(data);
+            })
             .catch(err => {
-                console.log("err", err)
-                res.json(err)
+                res.json(err);
             })
     });
 }
